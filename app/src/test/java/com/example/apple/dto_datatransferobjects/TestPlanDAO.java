ad493f18b6ff73710120a04ce1e5df0924b16cff
apple <hansp21c@gmail.com>
2017-09-12 20:45:33 -0500

    JUnit Test

---
+++ app/src/test/java/com/example/apple/dto_datatransferobjects/TestPlanDAO.java
@@ -0,0 +1,12 @@
+package com.example.apple.dto_datatransferobjects;
+
+/**
+ * Created by apple on 9/12/17.
+ */
+
+public class TestPlanDAO {
+
+    public void testPlantDAO_searchForRedbudShouldReturnAtLeastOneResult() {
+
+    }
+}
